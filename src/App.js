f61ac5ad86d736f1021fa091608b7d7683611487
minhogang <minhogang@yahoo.com>
2019-01-27 00:23:49 +1000

    Add dependiences

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
 import './App.css';
+import { TextField } from '@material-ui/core/TextField';
+import { Select } from '@material-ui/core/Select'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		age:'',
+		name: 'hai',
+		labelWidth: 0
+	}
+
+	componentDidMount() {
+
+	}
+	render() {
+		return (
+			<div className="App">
+				<Helmet>
+					<title>UOG Course Scheduler</title>
+					<meta
+						name="viewport"
+						content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+					/>
+					<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+
+				</Helmet>
+				<div>
+					<Select>
+						value=
+					</Select>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
